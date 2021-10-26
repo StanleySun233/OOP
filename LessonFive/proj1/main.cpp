4b54f2668cd9a0bf6f1b2ab6676bd6cfe5bd77b8
stanleysun233 <stanleysun233@163.com>
2021-10-26 21:14:08 +0800

    提交文件

--- LessonFive/proj1/main.cpp
+++ LessonFive/proj1/main.cpp
@@ -1,6 +1,34 @@
-#include <iostream>
+#include<iostream>
+#define MAXSIZE 100
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+class student
+{
+private:
+    static int count;
+    char name[MAXSIZE];
+public:
+    student(char n[])
+    {
+        strcpy_s(this->name,n);
+        count+=1;
+    }
+    static int pr()
+    {
+        return count;
+    }
+};
+int student::count = 0;
+int main()
+{
+    int t = 10;
+    while(t)
+    {
+        char a[MAXSIZE];
+        cin>>a;
+        student student(a);
+        cout<<"the number of the admit students is "<<student::pr()<<endl;
+        t--;
+    }
     return 0;
 }
