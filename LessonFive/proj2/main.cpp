2330840c3c6e3e4fff51403972b66e4a0cc87eb7
stanleysun233 <stanleysun233@163.com>
2021-10-26 21:20:58 +0800

    上传

---
+++ LessonFive/proj2/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+class M
+{
+private:
+    int a;
+    static int b;
+public:
+    M(int a0);
+    static void fun(M &m);
+};
+
+int M::b = 10;
+
+M::M(int a0)
+{
+    this->a = a0;
+}
+
+void M::fun(M &m)
+{
+    std::cout<<m.a<<" "<<M::b<<std::endl;
+}
+
+int main()
+{
+    M p(6),q(8);
+    M::fun(p);
+    M::fun(q);
+}
