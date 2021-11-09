3e85676c22c440981efd6c64821c538a73c9db81
stanleysun233 <stanleysun233@163.com>
2021-11-09 15:05:56 +0800

    push code on 2021.11.9

---
+++ LessonSix/cmake-build-debug/CMakeFiles/LessonSix.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LessonSix.dir/main.cpp.obj"
+  "LessonSix.exe"
+  "LessonSix.exe.manifest"
+  "LessonSix.lib"
+  "LessonSix.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LessonSix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ LessonSix/cmake-build-debug/CMakeFiles/LessonSix.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LessonSix.
+# This may be replaced when dependencies are built.
